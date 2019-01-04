3bd7f28020a5087ff0df2daa485a5d575e648317
Eric B Berger <ericb.berger@gmail.com>
2019-01-04 10:58:07 -0500

    file creation

---
+++ assignment-1.js
@@ -0,0 +1,24 @@
+var numbers = []
+
+for (var i=0; i < 10; i++){
+    /* your code here */
+}
+
+console.log(/* your code here */)
+console.log(/* your code here)
+
+var car = /* your code here */
+
+car.colour = /* your code here */
+
+
+
+
+Instructions
+Create a JavaScript file and name it assignment-1.js. Inside this file:
+Create a variable named `numbers` and assign an empty array to it.
+Using a for-loop and the Array push() method, insert the numbers 0 to 9 into the array you named numbers.
+Test that you used the array push method correctly by console logging the first item in the array.
+Console log the last item in the array.
+Create another variable named car and assign an empty object to it.
+Give the object a property called colour and assign it the value of "black".
